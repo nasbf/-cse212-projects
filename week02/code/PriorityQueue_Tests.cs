f9e4617be0bdf1c8aa42f325325cf5d49f68ff00
unknown <mo0ma@hotmail.com>
2026-07-11 16:57:25 -0500

    code queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,71 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Add an item (data and priority) to the queue
+    // Expected Result: The item added is in the back position in the queue.
+    // Defect(s) Found: None.
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("data 1", 3); 
+        var result = priorityQueue.Dequeue();
+        
+        Assert.AreEqual("data 1", result);
+        
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Add three items with different priorities. (priority 1,2,3) when dequeue runs obtain the higest priority item first (3) in this case. 
+    // Expected Result: item with priority 3.
+    // Defect(s) Found: do not examine all items in the queue, so the highestpriority item could be skipped.
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        priorityQueue.Enqueue("data 1", 1);
+        priorityQueue.Enqueue("data 2", 2);
+        priorityQueue.Enqueue("data 3", 3);
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("data 3", result);
+        
     }
+
+
+    [TestMethod]
+    // Scenario: Add four items where two items share the highest priority. Verify that the first one added (FIFO) is dequeued first.
+    // Expected Result: display the item with highest priority (3) and delete it from the queue.
+    // Defect(s) Found: the function Dequeue() did nor return the first item with the first highest priority, it used the simbol >= so it display the last item with the hihest priority, 
+    // also the function did not remove the item from the queue.
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+        priorityQueue.Enqueue("data 1", 1);
+        priorityQueue.Enqueue("data 2", 3);
+        priorityQueue.Enqueue("data 3", 2);
+        priorityQueue.Enqueue("data 4", 3);
+
+        var result = priorityQueue.Dequeue();
+        Assert.AreEqual("data 2", result);
+        var result2 = priorityQueue.Dequeue();
+        Assert.AreEqual("data 4", result2);
+    }
+
+
+    [TestMethod]
+    // Scenario: Add items where one of them has a 0 priority it indicates that it has an infinity number of turns, so if it is dequeued it should be added back to the queue.
+    // Expected Result: the item with 0 or less priority shold be added back to the queue.
+    // Defect(s) Found: None.
+
+    public void TestPriorityQueue_4()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        var exception = Assert.ThrowsException<InvalidOperationException>(
+        () => priorityQueue.Dequeue()
+        );
+
+        Assert.AreEqual("The queue is empty.", exception.Message);
+    }
+    
 
     // Add more test cases as needed below.
 }
--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -11,22 +11,69 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: The user specify an invalid size for the queue (<= 0).  The default value of 10 should be used.
+        // Expected Result: the value of max_size should be 10.
         Console.WriteLine("Test 1");
 
         // Defect(s) Found: 
-
+        var service = new CustomerService(0);
+        for (int i = 0; i < 10; i++) {
+            service.AddNewCustomer("Client", i.ToString(), "learning c#");
+            }   
+        service.AddNewCustomer("newClient", "123456", "testing");
+            
+        
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Add a new client to the queue.
+        // Expected Result: The queue should contain the new client.
         Console.WriteLine("Test 2");
 
         // Defect(s) Found: 
+        var newClient = new CustomerService(5);
+        newClient.AddNewCustomer("newClient", "123458", "test");
+        Console.WriteLine(newClient);
+        Console.WriteLine("=================");
+
+        // Test 3
+        // Scenario: Fill the queue to its maximum capacity and then add one more customer.
+        // Expected Result: Display an error message indicatingthat the queue is full.
+        Console.WriteLine("Test 3");
+
+        // Defect(s) Found: 
+        var fullQueue = new CustomerService(2);
+        fullQueue.AddNewCustomer("Client1", "123456", "learning c#");
+
+        fullQueue.AddNewCustomer("Client2", "123457", "learning c#");
+        fullQueue.AddNewCustomer("Client3", "123458", "learning c#");
+        Console.WriteLine("The Queue is full");
+        Console.WriteLine("=================");
+
+        // Test 4
+        // Scenario: Dequeue a customer from the queue.
+        // Expected Result: Display the details of the dequeued customer.
+        Console.WriteLine("Test 4");
+
+        // Defect(s) Found: 
+        var deleteClient = new CustomerService(3);
+        deleteClient.AddNewCustomer("Client1", "123456", "learning c#");
+        deleteClient.AddNewCustomer("Client2", "123457", "learning c#");    
+
+        deleteClient.ServeCustomer();
+        Console.WriteLine("=================");
+
+        // Test 5
+        // Scenario: Attempt to serve a customer when the queue is empty.
+        // Expected Result: Display a message indicating the queue is empty.
+        Console.WriteLine("Test 5");
+
+        // Defect(s) Found: 
+        var emptyQueue = new CustomerService(3);
+        emptyQueue.ServeCustomer();
 
         Console.WriteLine("=================");
+
 
         // Add more Test Cases As Needed Below
     }
@@ -65,19 +112,19 @@ public class CustomerService {
     /// Prompt the user for the customer and problem information.  Put the 
     /// new record into the queue.
     /// </summary>
-    private void AddNewCustomer() {
+    private void AddNewCustomer(string name, string accountId, string problem) {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize) {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
 
-        Console.Write("Customer Name: ");
-        var name = Console.ReadLine()!.Trim();
-        Console.Write("Account Id: ");
-        var accountId = Console.ReadLine()!.Trim();
-        Console.Write("Problem: ");
-        var problem = Console.ReadLine()!.Trim();
+        // Console.Write("Customer Name: ");
+        // var name = Console.ReadLine()!.Trim();
+        // Console.Write("Account Id: ");
+        // var accountId = Console.ReadLine()!.Trim();
+        // Console.Write("Problem: ");
+        // var problem = Console.ReadLine()!.Trim();
 
         // Create the customer object and add it to the queue
         var customer = new Customer(name, accountId, problem);
@@ -88,9 +135,14 @@ public class CustomerService {
     /// Dequeue the next customer and display the information.
     /// </summary>
     private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
+        if (_queue.Count == 0) {
+            Console.WriteLine("Queue is empty.");
+            return;
+        }
+        var customer = _queue[0];        
         Console.WriteLine(customer);
+        _queue.RemoveAt(0);
+         
     }
 
     /// <summary>
